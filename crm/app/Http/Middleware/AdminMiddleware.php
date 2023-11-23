da86b5733e5dd6893ae6dc583d54c7b8eb4806c4
Eduardo Araujo Mantovani <dudu_manto@hotmail.com>
2023-11-23 15:28:13 -0300

    Mudança do layout
    Mudança na tela de cadastro e de edição de cadastro

--- crm/app/Http/Middleware/AdminMiddleware.php
+++ crm/app/Http/Middleware/AdminMiddleware.php
@@ -9,12 +9,11 @@ class AdminMiddleware
 {
     public function handle($request, Closure $next)
     {
-        // Verifica se o usuário está autenticado e é um administrador
+     
         if (Auth::check() && Auth::user()->admin == 1) {
             return $next($request);
         }
 
-        // Redireciona para uma página não autorizada
         return redirect('/cadastro');
     }
 }
--- crm/resources/views/listausuarios.blade.php
+++ crm/resources/views/listausuarios.blade.php
@@ -24,19 +24,51 @@
     @endif
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.7.0/dist/css/bootstrap.min.css">
 
-    <body>
-    <div class="container" style="margin-top: 100px;min-height: 700px;margin-left:2%">
-    <div style="display: flex;">
-    <form action="{{route('post.search')}}" method="post">
+<body>
+<div class="container" style="margin-top: 100px;min-height: 700px;margin-left:4%">
+   <div>
+         <button class="btn btn-primary" style="background-color:#003078; color:white;" type="button" data-bs-toggle="offcanvas" data-bs-target="#staticBackdrop" aria-controls="staticBackdrop">
+            Menu
+        </button>
+
+            <div class="offcanvas offcanvas-start" data-bs-backdrop="static" tabindex="-1" id="staticBackdrop" aria-labelledby="staticBackdropLabel">
+                <div class="offcanvas-header">
+                    <h5 class="offcanvas-title" id="staticBackdropLabel">Menu</h5>
+                    <button type="button" class="btn-close" data-bs-dismiss="offcanvas" aria-label="Close"></button>
+                </div>
+                <div class="offcanvas-body">
+                  <i class="fa-solid fa-users"></i>  <a href="{{route('lista.usuarios')}}" style="color: black;">Lista e Edição de Usuários</a>
+                </div>                   
+            </div>
+                            
+    </div>
+    <div style="display: flex;justify-content: flex-end;margin-top: -38px;">
+        <form action="{{route('post.search')}}" method="post">
             @csrf
             <div class="form-group" style="display: flex;">
             <input type="text" name="search" class="form-control" style="width: 250px;" placeholder="Pesquisar">
-            <button type="submit" class="btn btn-primary" style="width: 150px;height: 36px;margin-left: 30px;"> Filtrar</button>
+            <button type="submit" class="btn btn-primary" style="width: 150px;height: 36px;margin-left: 30px; background-color:#003078; color:white;"> Filtrar</button>
             </div>
           
         </form>
-        <button onclick="downloadCSV()" class="btn btn-primary" style="width: 150px;height: 36px;margin-left: 30px;">Download CSV</button>
+        <button onclick="downloadCSV()" class="btn btn-primary" style="width: 150px;height: 36px;margin-left: 30px; background-color:#003078; color:white;">Download CSV</button>
     </div>
+    <!-- <div>
+        <button class="btn btn-primary" style="   margin-top: 110px; margin-left: 30px;" type="button" data-bs-toggle="offcanvas" data-bs-target="#staticBackdrop" aria-controls="staticBackdrop">
+         Menu
+        </button>
+
+                    <div class="offcanvas offcanvas-start" data-bs-backdrop="static" tabindex="-1" id="staticBackdrop" aria-labelledby="staticBackdropLabel">
+                        <div class="offcanvas-header">
+                            <h5 class="offcanvas-title" id="staticBackdropLabel">Menu</h5>
+                            <button type="button" class="btn-close" data-bs-dismiss="offcanvas" aria-label="Close"></button>
+                        </div>
+                        <div class="offcanvas-body">
+                            <i class="fa-solid fa-users"></i>  <a href="{{route('lista.usuarios')}}" style="color: black;">Lista e Edição de Usuários</a>
+                        </div>
+                    </div>
+                
+    </div> -->
        
 
         <!-- Tabela de Cadastros -->
